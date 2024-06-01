8afa729da01d2a79a24a118f88db3132c22c1265
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2024-06-01 17:00:06 +0530

    implemented the second api which returns the github id

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("Hello Keerthi Kumar N");
 })
 
+app.get("/github", (req, res) => {
+    res.send("keerthikumarn")
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
